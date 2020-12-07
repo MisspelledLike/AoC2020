91d9ee37d3c9f90a774e863b4afc49d40f0a3eac
MisspelledLike <75363619+MisspelledLike@users.noreply.github.com>
2020-12-07 11:03:55 +0100

    Update day6-2.py

--- day6/day6-2.py
+++ day6/day6-2.py
@@ -1,6 +1,5 @@
 name = 'inp.txt'
 handle = open(name)
-#list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 's', 't', 'u','v','w','x','y','z']
 counts = dict()
 row = 0
 count = 0
